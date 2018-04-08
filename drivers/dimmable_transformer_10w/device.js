e74e2334c86057d1ed0c176c7a23d6d29f206cdb
Johan Bendz <34488771+JohanBendz@users.noreply.github.com>
2018-04-08 15:33:42 +0200

    Delete device.js

--- drivers/dimmable_transformer_10w/device.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const ZigBeeLightDevice = require('homey-meshdriver').ZigBeeLightDevice;
-
-class DimmableTransformer extends ZigBeeLightDevice {
-
-}
-
-module.exports = DimmableTransformer;
